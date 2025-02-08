4f135c0d9d889a6b3853235f4fab5f84523cadf9
Matzen65 <matzeliasson@gmail.com>
2025-02-08 12:10:19 +0100

    veckouppgift5 uppgift 1.py

--- veckouppgift_5/veckouppgift5 uppgift 1.py
+++ veckouppgift_5/veckouppgift5 uppgift 1.py
@@ -1,41 +1,45 @@
-# Uppgift1:
+
+# Uppgift1
 # Skriv ner vad du tror kommer skrivas ut.
 # Skriv sedan in koden i din IDE, exakt som den står, och kör den.
 # Fick du samma resultat som du trodde? Om inte, varför?
 
-
 # 1 Vilka ekvivalensklasser har uttrycken?
-# 1a.
-# x > 100
-# Alla talen som är strikt större än 100
 
-# 1b.
-# y == 42
-# Tal som är lika med 42
+# 1a. x > 100
 
-# 1c.
-# len(text) >= 5
-# Om strängen eller listan har längden 5 eller är längre
+# Visar True om 'x' Är en int eller float som är större än 100,även flyttal
+# Visar False om 'x' Är int eller float som är lika med 100 eller mindre.
+# 1b. y == 42
+# Visar True om 'y' Är int eller float som är lika med 42
+# Visar False om 'y' Är int eller float som inte är lika 42.
 
-# 1d.
-# z == True
-# Om booleanen är True
+# 1c len(text) >= 5
+# Visar True om 'text' Alla strängar av längden 5 eller längre.
+# Visar False om 'text' Om strängen har en längd som är under 5.
 
-# 1e.
-# 8 < v < 16
-# v skall vara mellan 8 och 16, dvs strikt mindre 16 och över 8
+# 1d.  z == True
+# Visar True om 'z' boolean uttrycket är True.
+# Visar False om 'z' boolean uttrycket är False
+
+# 1e. 8 < v < 16
+
+# Visar True om 'v' Är int eller float är större än 8 samt mindre än 16.
+# Visar False om 'v' Är int eller float är lika med 8 eller mindre eller 16 eller större.
+# Booleska uttryck och ger också False.
 
 # 1f.
 # w == 32 or w == 64 or w == 128
-# Variablen w får endast anta värdena 32, 64 eller 128
+# Visar True om 'w' Är lika med int eller float som antar värdena 32, 64 eller 128.
+# Visar False om 'w' Är andra värden.
 
-# 1g.
-# if x < 5: … elif x < 10: … elif x < 15: … else
-# Det finns 4st ekvivalensklasser, dvs om:
-# 1: x är strikt mindre än 5
-# 2: x är strikt mindre än 10 men strörre eller lika med 5
-# 3: x är strikt mindre än 15 men strörre eller lika med 10
-# 4: När x antar andra värden ex strörre eller lika med 5 eller alla andra fall ex sträng
+# 1g. if x < 5:  elif x < 10:  elif x < 15:  else
+
+# Visar True för x < 5 Visar True om 'x' är Int eller float som är strikt mindre än 5.
+# Visar True för x < 10 visar True om 'x' är Int eller float, lika med 5 eller större och mindre än 10.
+# Visar True för x < 15 visar True om 'x' är Int eller float, lika med 10 eller större och mindre än 15.
+# Villkoret för att uppfylla else är Int eller float, lika med 15 eller större.
+
 
 
 # 2 Det har smugit sig in kommentarer i stället för kod på några ställen.
@@ -45,27 +49,23 @@
 def sum_list(list):
     return None
 
-
 def test_empty_list():
     expected = 0
-    actual = sum_list([])
+    actual   = sum_list([])
     assert actual == expected
-
 
 def test_number_list():
     assert sum_list([5]) == 5
-    assert sum_list([1, 2, 3, 4, 5]) == 15
-    assert sum_list([0, 0, 0, 0, 0]) == 0
-    assert sum_list([-1, -2, -3, -4, -5]) == -15
+    assert sum_list([1,2,3,4,5]) == 15
+    assert sum_list([0,0,0,0,0]) == 0
+    assert sum_list([-1,-2,-3,-4,-5]) == -15
 
 
-# 3a Diskutera följande kod.
-# Räcker det med ett testfall för att testa funktionen?
+# 3a Diskutera följande kod, räcker det med ett testfall för att testa funktionen?
 
 # Returnerar ett heltal med antalet vokaler som finns i ordet (aeiouyåäö)
 def count_vowels(word):
-    return 0
-
+    return None
 
 def test_no_vowels():
     assert count_vowels("qwrt") == 0
@@ -73,53 +73,45 @@ def test_no_vowels():
     assert count_vowels("123 123") == 0
     assert count_vowels("") == 0
 
+# Assert kommandona funkar men syns inte utfallet för "de andra" assert testerna.
 
-# Själva testerna i sig dvs assert kommandona ser bra ut
-# men skulle säkert behöva delas upp i olika tester,
-# för att om något test fallerar så ser man inte hur de andra assert testernapip pytest
-# gick
+
+# 3b Skriv färdigt funktionen count_vowels med hjälp av TDD-metoden, red → green → refactor.
+
+# Returnerar ett heltal med antalet vokaler som finns i ordet (aeiouyåäö)
+def count_vowels(word):
+    return 0
+
+def test_no_vowels():
+    assert count_vowels("qwrt") == 0
+    assert count_vowels("Tt") == 0
+    assert count_vowels("123 123") == 0
+    assert count_vowels("") == 0
+
+# Testet returnerar 0 då testet går igenom.
+# Man får sedan skriva tillräckligt med tester för refactor
+
 
 # 4 Formulera testfall för en funktion som hittar största talet i en lista.
 
-# Returnerar det största talet i listan
-# Returnerar None om det inte finns något
+# Returnera det största talet i listan
+# Returnera None om det inte finns något
 def find_max(list):
     None
 
-
-def test_greatest_number_in_list():
-    excepted = 80
-    actual = find_max([60, 30, 80, 1])
-
+def test_find_biggest_number_list():
+    excepted = 38
+    actual = find_max([16, 38, 1])
     assert actual == excepted
 
-
-def test_greatest_number_in_list_is_none_when_list_is_empty():
+def test_find_biggest_number_list_show_none_when_empty():
     excepted = None
     actual = find_max([])
-
     assert actual == excepted
 
 
 # 5 Winner takes it all brukar det ju heta, men nu ska vi försöka ge lite heder åt alla andrapristagare.
 # Formulera testfall för en funktion som hittar näst största talet i en lista!
-
 # Returnerar det nästa största talet i listan
 # Returnerar None om det inte finns något
 # Om det är delad förstaplats så returneras det talet
-def find_2nd_max(list):
-    None
-
-
-def test_2nd_greatest_number_in_list():
-    excepted = 60
-    actual = find_2nd_max([60, 30, 80, 1])
-
-    assert actual == excepted
-
-
-def test_2nd_greatest_number_in_list_is_none_when_list_is_empty():
-    excepted = None
-    actual = find_2nd_max([])
-
-    assert actual == excepted
